7e6850af63121eaa2f824d2cb06ae205bdec0d9b
Gonzalo Liano ricoy <goliano-@c1r14s6.42madrid.com>
2022-02-07 13:21:52 +0100

    init_prompt

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: goliano- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 11:33:34 by goliano-          #+#    #+#             */
+/*   Updated: 2022/02/07 13:18:26 by goliano-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -14,9 +26,16 @@
 # define BUFFER_SIZE 100
 # endif
 
+typedef struct g_data
+{
+	char	*prompt;
+	char	**envp;
+}t_gdata;
+
 char	*get_next_line(int fd);
 size_t	ft_strlen(char const *str);
 char	*ft_strjoin(char const *str1, char const *s2);
 char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,6 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: goliano- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 11:33:37 by goliano-          #+#    #+#             */
+/*   Updated: 2022/02/07 13:21:33 by goliano-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-void	exec_command(char *cmd, char **envp)
+char	*remove_new_line(char *str)
+{
+	int		i;
+	char	*word;
+
+	i = 0;
+	word = malloc(sizeof(char) * ft_strlen(str));
+	if (!word)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			break ;
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	free(str);
+	return (word);
+}
+
+char	*exec_command(char *cmd, char **envp)
 {
 	int	end[2];
 	pid_t	p;
@@ -8,9 +41,8 @@ void	exec_command(char *cmd, char **envp)
 
 	pipe(end);
 	p = fork();
-	printf("STDOUT: %d\n", STDOUT_FILENO);
 	if (p < 0)
-		return (perror("Fork: "));
+		return (0);
 	if (p == 0)
 	{
 		close(end[0]);
@@ -24,21 +56,77 @@ void	exec_command(char *cmd, char **envp)
 	close(end[1]);
 	waitpid(p, NULL, 0);
 	char *out = malloc(sizeof(char) * 4096);
-	read(end[0], out, 4096);
-	printf("OUT: %s\n", out);
+	out = get_next_line(end[0]);
+	out = remove_new_line(out);
 	close(end[0]);
+	return (out);
+}
+
+char	*add_at_sign(char *str)
+{
+	char	*word;
+	int		i;
+
+	word = malloc(sizeof(char) * ft_strlen(str + 1));
+	if (!word)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '@';
+	word[i + 1] = '\0';
+	free(str);
+	return (word);
+}
+
+char *pretty_hostname(char *str)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc(sizeof(char) * ft_strlen(str + 3));
+	if (!word)
+		return (0);
+	while(str[i])
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = ':';
+	word[i + 1] = ' ';
+	word[i + 2] = '\0';
+	free(str);
+	return (word);
+}
+
+void	init_prompt(t_gdata *g_data, char **envp)
+{
+	char	*username;
+	char	*hostname;
+
+	g_data->envp = envp;
+	username = exec_command("/usr/bin/whoami", envp);
+	username = add_at_sign(username);
+	hostname = exec_command("/bin/hostname", envp);
+	hostname = pretty_hostname(hostname);
+	g_data->prompt = ft_strjoin(username, hostname);
 }
 
 int main(int argc, char **argv)
 {
 	char		*inp;
 	extern char	**environ;
+	t_gdata		g_data;
 	
 	printf("ARGC: %d\n", argc);
 	printf("ARGV[0]: %s\n", argv[0]);
-	inp = readline("amnesia@:");
+	init_prompt(&g_data, environ);
+	inp = readline(g_data.prompt);
 	printf("INP: %s\n", inp);
-	exec_command("/usr/bin/whoami", environ);
 	//execve("/usr/bin/whoami", cmd, environ);
 	//execve("/usr/bin/hostnamectl", cmd, environ);
 	/*while (42)
